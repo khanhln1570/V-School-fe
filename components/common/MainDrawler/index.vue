219e78e7c21eecbca7e471b49504e864190f9b8e
Lam Nguyen <lam.nguyen@madison-technology.com>
2021-03-04 12:12:03 +0700

    Reuse component => main drawler

---
+++ components/common/MainDrawler/index.vue
@@ -0,0 +1,74 @@
+<template>
+<v-navigation-drawer v-model="myDrawler" :fixed="fixed" :app="app" :expand-on-hover="expandOnHover" :temporary="temporary" :right="right">
+  <v-list v-if="items">
+    <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+      <v-list-item-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.title" />
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+
+  <slot name="custom-list"></slot>
+</v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    drawer: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    fixed: {
+      type: Boolean,
+      default: false,
+    },
+    app: {
+      type: Boolean,
+      default: false,
+    },
+    expandOnHover: {
+      type: Boolean,
+      default: false,
+    },
+    items: {
+      type: [Object, Array],
+      default: null,
+    },
+    temporary: {
+      type: Boolean,
+      default: false,
+    },
+    right: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    myDrawler: {
+      // getter
+      get: function () {
+        return this.drawer;
+      },
+      // setter
+      set: function (newValue) {
+        if(!newValue) {
+          this.$emit('clickOffDrawler', newValue);
+        }
+      }
+    },
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,83 +1,52 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-        <v-list-item @click.native="handleLogout">
-          <v-list-item-action>
-            <v-icon light> mdi-logout </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Logout</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+<v-app dark>
+  <CommonMainDrawler :drawer="drawer" :items="items" :fixed='true' :app='true' :expandOnHover='true' />
+  <v-app-bar fixed app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-spacer />
+    <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-app-bar>
+  <v-main>
+    <v-container>
+      <nuxt />
+    </v-container>
+  </v-main>
+  <CommonMainDrawler :drawer="rightDrawer" :right="right" :fixed='true' :temporary='true' @clickOffDrawler="onClick">
+    <template v-slot:custom-list>
+    <v-list>
+      <v-list-item to="/profile">
+        <v-list-item-action>
+          <v-icon light> mdi-account-arrow-left </v-icon>
+        </v-list-item-action>
+        <v-list-item-title>Profile</v-list-item-title>
+      </v-list-item>
+      <v-list-item @click.native="handleLogout" to="#">
+        <v-list-item-action>
+          <v-icon light> mdi-logout </v-icon>
+        </v-list-item-action>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </template>
+  </CommonMainDrawler>
+  <v-footer :absolute="!fixed" app>
+    <span>&copy; {{ new Date().getFullYear() }}</span>
+  </v-footer>
+</v-app>
 </template>
 
 <script>
-import {CLEAR_AUTH_MUTATION} from '~/store/auth/auth.constants';
+import {
+  CLEAR_AUTH_MUTATION
+} from '~/store/auth/auth.constants';
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
+      drawer: true,
       fixed: false,
-      items: [
-        {
+      items: [{
           icon: "mdi-apps",
           title: "Welcome",
           to: "/",
@@ -88,7 +57,16 @@ export default {
           to: "/inspire",
         },
       ],
-      miniVariant: false,
+      userItems: [{
+          icon: "mdi-repeat",
+          title: "Switch drawer (click me)",
+        },
+        {
+          icon: "mdi-logout",
+          title: "Logout",
+          to: "/inspire",
+        },
+      ],
       right: true,
       rightDrawer: false,
       title: "Vuetify.js",
@@ -97,6 +75,9 @@ export default {
   methods: {
     handleLogout() {
       this.$store.commit(CLEAR_AUTH_MUTATION)
+    },
+    onClick(payload) {
+      this.rightDrawer = payload;
     }
   }
 };

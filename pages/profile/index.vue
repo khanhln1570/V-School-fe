ff6fc7196d06e4ab3dff50863527b1cb77d2a2c6
Lam Nguyen <lam.nguyen@madison-technology.com>
2021-03-08 16:28:15 +0700

    Profile page

---
+++ pages/profile/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <v-row no-gutters>
+      <v-col cols="12" md="2"> </v-col>
+      <v-col cols="12" md="8">
+        <v-card class="pa-2 rounded-lg" outlined tile>
+          <v-card-title>
+            <p class="font-weight-bold title">My Account</p>
+          </v-card-title>
+          <v-card-text>
+            <CommonMainInput
+              label="Name"
+              name="name"
+              type="name"
+              v-model.trim="$v.form.name.$model"
+              :validation="$v.form.name"
+            />
+            <CommonMainInput
+              label="Email"
+              name="email"
+              type="email"
+              v-model.trim="$v.form.email.$model"
+              :validation="$v.form.email"
+            />
+            <CommonMainInput
+              label="Password"
+              name="password"
+              type="password"
+              v-model.trim="$v.form.password.$model"
+              :validation="$v.form.password"
+            />
+            <CommonMainInput
+              label="Confirm password"
+              name="confirmPassword"
+              type="password"
+              v-model.trim="$v.form.confirmPassword.$model"
+              :validation="$v.form.confirmPassword"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="2"> </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import {editProfile} from '~/validations/profile/profile.validation';
+export default {
+  data() {
+    return {
+      form: {
+        name: null,
+        email: null,
+        password: null,
+        confirmPassword: null,
+      },
+    }
+  },
+  validations : {
+    form : editProfile
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ validations/profile/profile.validation.js
@@ -0,0 +1,53 @@
+import {
+	required,
+	email,
+	minLength,
+  sameAs,
+  alpha
+} from "vuelidate/lib/validators";
+
+const nircNumber = /^[0-9]*$/;
+
+const validNric = (val) => {
+	const test = nircNumber.test(val);
+	
+	if (!test) return false;
+	
+	if (val.length !== 12) return false;
+
+	return true;
+}
+
+const minAge = (val) => {
+	const age = val.slice(0, 2);
+	if (age < 21) return false;
+
+	return true;
+}
+
+const phoneNumber = (val) => {
+	// if (val.slice(0, 3) !== '+60') return false;
+	// const phoneRegex = /^[0-9]*$/;
+	// const phoneWithoutCode = val.replace('+60', '');
+	// const test = phoneRegex.test(phoneWithoutCode);
+	// if (!test) return false;
+	if (val.length > 16 || val.length < 10) return false;
+	return true;
+}
+
+export const editProfile = {
+  name: {
+    required,
+    alpha: alpha,
+  },
+  email: {
+    required,
+    email,
+  },
+  password: {
+    password: minLength(8),
+  },
+  confirmPassword: {
+    confirmPassword: sameAs("password"),
+  }
+};

b12287348519678f1084213e21fc8f928fe74b43
hung110300 <hung110300@gmail.com>
2021-05-18 20:15:22 +0700

    fix layout

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,106 +1,46 @@
 <template>
-  <div class="bg-color">
-    <img src="../assets/images/login/bg-login-mountain.svg" class="overlay" />
-    <img src="../assets/images/login/bg-login-book.svg" class="overlay-book" />
-    <img
-      src="../assets/images/login/bg-login-moon.svg"
-      class="overlay-moon"
-      :class="{
-        'd-none ': $vuetify.breakpoint.mobile,
-      }"
-    />
-    <img src="../assets/images/login/bg-login-tree.svg" class="overlay-tree" />
-
-    <div class="rules pa-2">
-      <span class="pr-5 white--text" style="border-right: 1px solid"
-        >Chính sách</span
+  <div>
+    <group-validator
+      class="mt-md-3 mt-xl-10 px-xl-8"
+      :validation="$v.form.email"
+    >
+      <template slot-scope="{ errors }">
+        <label>Tên đăng nhập <span class="red--text">*</span></label>
+        <main-input
+          placeholder=""
+          name="email"
+          type="email"
+          v-model.trim="$v.form.email.$model"
+          :errors="errors"
+        />
+      </template>
+    </group-validator>
+    <group-validator class="mt-xl-1 px-xl-8" :validation="$v.form.password">
+      <template slot-scope="{ errors }">
+        <label>Mật khẩu <span class="red--text">*</span></label>
+        <main-input
+          placeholder=""
+          name="password"
+          type="password"
+          v-model.trim="$v.form.password.$model"
+          :errors="errors"
+        />
+      </template>
+    </group-validator>
+    <div class="px-xl-8">
+      <v-btn
+        class="mt-md-4 mt-xl-12"
+        block
+        large
+        color="primary"
+        @click="onSubmit"
+        >Đăng nhập</v-btn
       >
-      <span class="pl-4 white--text">Điều khoản</span>
     </div>
-
-    <div>
-      <div
-        class="signin-container d-flex justify-center align-center flex-column-reverse flex-md-row-reverse"
+    <div class="mt-1 text-right">
+      <nuxt-link to="reset-password" class="txt-secondary--text"
+        ><small class="reset-password px-xl-8">Quên mật khẩu?</small></nuxt-link
       >
-        <div></div>
-        <div class="col-md-4 col-xl-4 px-xl-10 pt-md-15 h-100">
-          <div
-            class="d-flex justify-space-around align-center flex-column h-100"
-            style="padding-bottom: 15rem"
-          >
-            <img
-              src="../assets/images/login/bg-login-logo.svg"
-              class="overlay-logo mb-xl-5 mt-5 mb-md-10"
-            />
-            <v-card
-              class="card rounded-lg signin-left py-xl-10 px-md-8 py-md-8"
-              elevation="11"
-              :class="{
-                'pa-8 ': $vuetify.breakpoint.mobile,
-              }"
-              width="500px"
-            >
-              <div class="d-flex justify-center align-center text-right">
-                <div class="">
-                  <div class="display-2 font-weight-normal">V-School</div>
-                  <div>
-                    <small>beta</small>
-                  </div>
-                </div>
-              </div>
-              <group-validator
-                class="mt-xl-10 mt-md-3 px-xl-8"
-                :validation="$v.form.email"
-              >
-                <template slot-scope="{ errors }">
-                  <label>Tên đăng nhập <span class="red--text">*</span></label>
-                  <main-input
-                    placeholder=""
-                    name="email"
-                    type="email"
-                    v-model.trim="$v.form.email.$model"
-                    :errors="errors"
-                  />
-                </template>
-              </group-validator>
-              <group-validator
-                class="mt-xl-1 px-xl-8"
-                :validation="$v.form.password"
-              >
-                <template slot-scope="{ errors }">
-                  <label>Mật khẩu <span class="red--text">*</span></label>
-
-                  <main-input
-                    placeholder=""
-                    name="password"
-                    type="password"
-                    v-model.trim="$v.form.password.$model"
-                    :errors="errors"
-                  />
-                </template>
-              </group-validator>
-              <div class="px-xl-8">
-                <v-btn
-                  class="mt-xl-12 mt-md-4"
-                  block
-                  large
-                  color="primary"
-                  @click="onSubmit"
-                  >Đăng nhập</v-btn
-                >
-              </div>
-              <div class="mt-1 text-right">
-                <nuxt-link to="reset-password" class="txt-secondary--text"
-                  ><small class="reset-password px-xl-8"
-                    >Quên mật khẩu?</small
-                  ></nuxt-link
-                >
-              </div>
-            </v-card>
-          </div>
-        </div>
-        <div></div>
-      </div>
     </div>
   </div>
 </template>

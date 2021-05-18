0633bb3ce5815a3ba2484e9141efe0631c349cf1
hung110300 <hung110300@gmail.com>
2021-05-18 20:20:02 +0700

    create your-child

---
+++ pages/your-child.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <p class="mt-6">Con của bạn:</p>
+    <v-alert
+      v-for="(child, index) in children"
+      :key="index"
+      outlined
+      class="mt-xl-5 mt-md-3 px-xl-3 py-xl-4"
+    >
+      <div class="d-flex justify-space-between">
+        <div class="ml-1 font-weight-medium">
+          <span
+            class="dot mr-2"
+            :class="{ green: child.isActive, red: !child.isActive }"
+          ></span>
+          {{ child.name }}
+        </div>
+        <span>{{ child.address }}</span>
+      </div>
+    </v-alert>
+    <span class="dot"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "auth",
+  computed: {
+    children() {
+      return this.$store.state.yourChild.children;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../pages/__styles/signin.scss";
+.dot {
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
---
+++ store/yourChild/state.js
@@ -0,0 +1,7 @@
+export default () => ({
+    children: [
+        { id: 1, name: 'Trần Thị Tố Trinh', address: "8C5 Hoàng Diệu", classCode: "10A9", schoolId: "DE12", status: "", isActive: true },
+        { id: 2, name: 'Nguyễn Quốc Cường', address: "365 Nguyễn Tri Phương", classCode: "10A12", schoolId: "DE12", status: "", isActive: true },
+        { id: 3, name: 'Khánh Mooi', address: "12 Phan Bội Châu", classCode: "10A15", schoolId: "DE12", status: "", isActive: false }
+    ]
+});

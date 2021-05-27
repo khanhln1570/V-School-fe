406bed9d1960beccc1b78626da58334236b29284
hung110300 <hung110300@gmail.com>
2021-05-27 22:22:45 +0700

    invoice manage page

---
+++ components/invoice-manage/invoice-manage-row/InvoiceManageRow.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-sheet
+      v-if="invoice.status === 'PENDING'"
+      elevation="0"
+      class="invoiceManageRow mb-2 px-5 py-3"
+    >
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="(item, index) in headers"
+            :key="index"
+            class="pa-0 d-flex align-center"
+            :class="{
+              'd-flex justify-end': index === headers.length - 1,
+            }"
+          >
+            <slot
+              :name="item.value"
+              :value="invoice[item.value]"
+              :item="invoice"
+            >
+              {{ invoice[item.value] }}
+              {{ item.value }}
+            </slot>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      default: () => [],
+    },
+    invoice: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./InvoiceManageRow.scss";
+</style>
--- pages/invoices.vue
+++ pages/invoices.vue
@@ -8,13 +8,33 @@
 
     <main-tabs :items="tabItem">
       <template #tabRight>
-        <table-search :search.sync="search" placeHolder="Hãy nhập gì đó …" @searchChange="handleChangeSearch"></table-search>
+        <table-search
+          :search.sync="search"
+          placeHolder="Hãy nhập gì đó …"
+          @searchChange="handleChangeSearch"
+        ></table-search>
       </template>
 
-      <template #table>
-
-        <p v-for="(invoice, index) in invoices" :key="index"> {{invoice}}</p>
-
+      <template #incoming>
+        <!-- <p v-for="(invoice, index) in invoices" :key="index">{{ invoice }}</p> -->
+        <div class="mt-10">
+          <div v-for="(type, index) in invoiceTypes" :key="index">
+            <invoice-manage-group
+              :invoices="invoices"
+              :invoiceType="type"
+              :headers="headers"
+            >
+            </invoice-manage-group>
+          </div>
+        </div>
+      </template>
+      <template #history>
+        <div class="mt-10">
+          <div v-for="(type, index) in invoiceTypes" :key="index">
+            <invoice-manage-group :invoices="invoices" :invoiceType="type" :headers="headers">
+            </invoice-manage-group>
+          </div>
+        </div>
       </template>
     </main-tabs>
   </v-container>
@@ -23,16 +43,25 @@
 <script>
 import { mapGetters } from "vuex";
 
+
 export default {
   components: {
+    InvoiceManageGroup: () =>
+      import(
+        "@/components/invoice-manage/invoice-manage-group/InvoiceManageGroup"
+      ),
+
     MainTable: () => import("@/components/commons/main-table/MainTable"),
     TableSearch: () => import("@/components/commons/main-table/TableSearch"),
     PageHeader: () => import("@/components/commons/page-header/PageHeader"),
     MainSelect: () => import("@/components/commons/main-select/MainSelect"),
     MainTabs: () => import("@/components/commons/main-tabs/MainTabs"),
-    CusIconTextButton: () => import("@/components/commons/main-button/cus-icon-text-button/CusIconTextButton"),
+    CusIconTextButton: () =>
+      import(
+        "@/components/commons/main-button/cus-icon-text-button/CusIconTextButton"
+      ),
     TextButton: () =>
-      import("@/components/commons/main-button/text-button/TextButton"),
+      import("@/components/commons/main-button/text-button/TextButton")
   },
   data() {
     return {
@@ -47,13 +76,28 @@ export default {
         },
       ],
       selected: [],
-      search: '',
+      search: "",
+      headers: [
+        {
+          text: "Số tiền",
+          value: "total",
+        },
+        {
+          text: "Học sinh",
+          value: "student",
+        },
+        {
+          text: "",
+          value: "action",
+        },
+      ],
     };
   },
   computed: {
     ...mapGetters({
       invoices: "invoice/getInvoices",
       count: "invoice/getCountSchool",
+      invoiceTypes: "invoice/getInvoiceTypes",
     }),
   },
   methods: {
@@ -65,11 +109,11 @@ export default {
     },
     handleViewClick(item) {
       console.log(item);
-    }
+    },
   },
   async created() {
     await this.$store.dispatch("invoice/getInvoices");
-  }
+  },
 };
 </script>
 

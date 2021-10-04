3fca2539652f7902c19f3fffb326c9522cf4dd5e
Lam Nguyen <lam.nguyen@madison-technology.com>
2021-10-04 18:30:55 +0700

    add asset

---
+++ pages/payment/result.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="px-xl-8 d-flex flex-column justify-center">
+    <div class="d-flex flex-column justify-center align-center mt-5" v-if="$route.query.status=== 'success'">
+      <h3 class="mb-5">Thanh toán hoá đơn thành công</h3>
+      <img width="100px" src="@/assets/images/checked.svg" alt="checked">
+    </div>
+    <div class="d-flex flex-column justify-center align-center mt-5" v-else>
+      <h3 class="mb-5">Thanh toán hoá đơn thất bại</h3>
+      <img width="100px" src="@/assets/images/cancel.svg" alt="cancel">
+    </div>
+    <v-btn to="/" color="primary" class="mt-5 mx-auto">Trang chủ</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "auth",
+  middleware: "",
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  fetch() {
+    console.log(this.$route.query.status);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../__styles/auth.scss";
+</style>
